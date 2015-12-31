c577916de6ab72bc0444364e3c1d391f474b2a37
JorelLatraille <jlatraille@googlemail.com>
2015-12-31 12:25:40 +0000

    finished adding larger images for thumbnails/work-belt

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
 
   smoothScroll(500);
   workBelt();
+  workLoad();
 
 });
 
@@ -30,6 +31,24 @@ function workBelt(){
     $('.work-return').click(function() {
         $('.work-belt').css('left', '0%');
         $('.work-container').hide(800);
+    });
+    
+}
+
+function workLoad() {
+    
+    $.ajaxSetup({ cache: true });
+    
+    $('.thumb-unit').click(function() {
+        
+        var $this = $(this),
+            newTitle = $this.find('strong').text(),
+            newFolder = $this.data('folder'), 
+            spinner = '<div class="loader">Loading...</div>',
+            newHTML = '/work/' + newFolder + '.html';
+        $('.project-load').html(spinner).load(newHTML);
+        $('.project-title').text(newTitle);
+        
     });
     
 }
